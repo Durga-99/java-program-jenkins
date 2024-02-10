69002eb9e3b3f5ba8c92303113f94f10ba1af588
Durga <durgasujatha9299@gmail.com>
2024-02-10 20:54:23 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class main{
+    public static void main (string [] args){
+        
+    system.out.println(" his is my first java code");
+    system.out.println(" its always best to learn new";)
+    system.out.println(new Date());
+    }
+}
---
+++ main1.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class main1{
+    public static void main(String[] args) {
+        System.out.println("This is my first Java code");
+        System.out.println("It's always best to learn new");
+        System.out.println(new Date());
+    }
+}
